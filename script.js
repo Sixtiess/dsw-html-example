188a5aed1b1c51a312af85cadfbe3662a18d84dc
sixtiess <liamhhickey@icloud.com>
2024-08-23 13:01:18 -0700

    latest

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+var isText = false;
+
 function textAppear(){
   var text = document.getElementById("txt");
-  text.style.display = "block";
+  if(isText == false){
+    text.style.display = "block";
+    isText = true;
+  } else {
+    text.style.display = "none";
+    isText = false;
+  }
 }
